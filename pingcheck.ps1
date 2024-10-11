c8b3a4bf71fb308634f9c43ce31512055dd4367d
chasetimmons <38573434+chasetimmons@users.noreply.github.com>
2024-10-11 09:19:54 -0700

    Added more comments

--- pingcheck.ps1
+++ pingcheck.ps1
@@ -1,24 +1,30 @@
 ##########################################################
 #  // pingcheck.ps1	by Chase Timmons		 #
-#  //                                           	 #
+#  // Ping a list of addresses to test connectivity      #
 #  //  	 	                                         #
 ##########################################################
 
-# List of Addresses by Name/IP	  
+# List of addresses by name/IP
+# Add additional nodes here
 $items = @(
 	[pscustomobject]@{Name="Location1";IP="1.1.1.1"}
 	[pscustomobject]@{Name="Location2";IP="1.1.1.2"}
 	[pscustomobject]@{Name="Location3";IP="1.1.1.3"}
 )
 
+#Reset the counter
 $count = 0
 
-Clear-Host		
+# Clear the terminal window
+Clear-Host	
+# Print the title
 "Ping Check"
+# Print the date
 Get-Date
 "================================================"
 
 # Loop through addresses and ping them once
+# Incread -Count for additional pings
 foreach ($i in $items)
 {
 	$count += 1
@@ -30,5 +36,5 @@ foreach ($i in $items)
 }
 
 "================================================"
-
+# Don't automatically close the window when done
 pause
